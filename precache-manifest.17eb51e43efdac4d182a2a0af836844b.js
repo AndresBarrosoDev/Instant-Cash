924649bb5a34e6ca11ce39ceee795e821d4a44bb
Andres Barroso <andres.barroso@gmail.com>
2021-12-05 15:21:21 -0300

    Complete build deployed to gh-pages 0.3

---
+++ precache-manifest.17eb51e43efdac4d182a2a0af836844b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0442a6b8eb740810c876",
+    "url": "/Instant-Cash/css/app.ea70bc1b.css"
+  },
+  {
+    "revision": "5b3ebe7bddf22b292051",
+    "url": "/Instant-Cash/css/chunk-vendors.3fc1e342.css"
+  },
+  {
+    "revision": "662bacbcb515a827b00249dee61353de",
+    "url": "/Instant-Cash/img/coinbanner2.2.662bacbc.jpg"
+  },
+  {
+    "revision": "d012a45f8039eefab29306e0091fd368",
+    "url": "/Instant-Cash/img/logo.d012a45f.png"
+  },
+  {
+    "revision": "9c7c80ec537c13d04a1a8faf4c6b9579",
+    "url": "/Instant-Cash/index.html"
+  },
+  {
+    "revision": "0442a6b8eb740810c876",
+    "url": "/Instant-Cash/js/app.427d6997.js"
+  },
+  {
+    "revision": "5b3ebe7bddf22b292051",
+    "url": "/Instant-Cash/js/chunk-vendors.4e5bf97b.js"
+  },
+  {
+    "revision": "634067bafbd9e9311d8732cb95e23d4f",
+    "url": "/Instant-Cash/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Instant-Cash/robots.txt"
+  }
+]);
